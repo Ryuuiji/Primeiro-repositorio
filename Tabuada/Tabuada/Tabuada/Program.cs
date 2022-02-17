1b5a1ea234962e6c5008cf08a70db3a27cca6037
Ryuuiji <leosilvaryu@hotmail.com>
2022-02-17 16:25:49 -0300

    Inclusão de um codigo para fazer tabuadas

---
+++ Tabuada/Tabuada/Tabuada/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Olá, digite o numero da tabuada que deseja: ");
+            int nume = Convert.ToInt32 (Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(nume + " x " + i + " = " + (nume * i));
+            }
+        }
+    }
+}
